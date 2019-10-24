a922115f5527628a8d3186e14caddd0af87d9c71
Huang Wenfeng <Wenfeng.Huang@desay-svautomotive.com>
2019-10-24 17:13:18 +0800

    feat:占位符使用

---
+++ src/main/java/com/summary/java/base/Placeholder.java
@@ -0,0 +1,26 @@
+package com.summary.java.base;
+
+import org.junit.Test;
+
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * 
+ * @Description： 占位符使用
+ * 
+ * @author [ Wenfeng.Huang ] on [2019年8月23日下午6:37:25]
+ * @Modified By： [修改人] on [修改日期] for [修改说明]
+ *
+ */
+public class Placeholder {
+
+	@Test
+	public void test() {
+		System.err.println(StrFormat("ID"));
+	}
+
+	public String StrFormat(Object... str) {
+		String msg = "请求参数[{}]错误";
+		return StrUtil.format(msg, str);
+	}
+}
