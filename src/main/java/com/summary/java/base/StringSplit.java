3faee5d8800a260ba89248adcc21bd6b279a1c25
Huang Wenfeng <Wenfeng.Huang@desay-svautomotive.com>
2020-03-31 16:12:04 +0800

    feat:字符串分割

---
+++ src/main/java/com/summary/java/base/StringSplit.java
@@ -0,0 +1,26 @@
+package com.summary.java.base;
+
+import org.junit.Test;
+
+public class StringSplit {
+
+	@Test
+	public void test() {
+		int len = 10;
+		String str = "a,b,c,d,e,f,g,h,i,j,k,m,n,o,p,q,w,y,z,x,2,9";
+		String[] arr = str.split(",");
+		int loop = str.length() / len;
+		for (int i = 0; i < loop; i++) {
+			String result = "";
+			for (int j = 0; j < len; j++) {
+				int index = i * len + j;
+				if (index >= arr.length) {
+					break;
+				}
+				result += arr[index];
+			}
+			System.out.println("====" + result);
+		}
+
+	}
+}
